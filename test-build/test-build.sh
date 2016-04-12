3ecf8d22c74eb3e5f467a543518882b31e5b1b53
yuhuayun <3246068556@qq.com>
2016-04-13 02:52:50 +0800

    new_1

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -9,5 +9,5 @@ sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
 chmod 777 ./rancher-compose
 
-./rancher-compose --access-key 824A727726D9C629418F --secret-key US4snt2WHioPPJ1dCXrxpyjzQ6UYbaoZ434iKNhX -p python-redis-demo-build${BUILD_NUMBER} up -d
+./rancher-compose --access-key 3066B53A898844468011 --secret-key RiehfecdpYi7Jf8p3UNkimXSt8wR7DomZSZ98oyZ -p python-redis-demo-build${BUILD_NUMBER} up -d
 
