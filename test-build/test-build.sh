fa3131fa4e91a5e0454d2494660bc5e11fe5da8c
yuhuayun <3246068556@qq.com>
2016-04-13 00:38:21 +0800

    new

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 cd ${WORKSPACE}/src
-docker build -t 192.168.0.35:5000/python-redis-demo:${BUILD_NUMBER} .
+docker build -t 192.168.0.35:80/python-redis-demo:${BUILD_NUMBER} .
 
-docker push 192.168.0.35:5000/python-redis-demo:${BUILD_NUMBER}
+docker push 192.168.0.35:80/python-redis-demo:${BUILD_NUMBER}
 cd ${WORKSPACE}/test-build
 
 sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
 chmod 777 ./rancher-compose
 
-./rancher-compose --access-key 2C90A63D9841988CD3D1 --secret-key yKZnC4ntKMLVRgqxWtAUn33HzmZd76KngGzVRQtu -p python-redis-demo-build${BUILD_NUMBER} up -d
+./rancher-compose --access-key A334716B54CF92223F44 --secret-key hpxZC5qU5aJzpA8j3uAFHbKByvBr9hgcnQ17DQzp -p python-redis-demo-build${BUILD_NUMBER} up -d
 
